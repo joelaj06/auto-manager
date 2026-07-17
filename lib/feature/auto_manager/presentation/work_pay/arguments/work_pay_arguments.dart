b3812fae51168b551ad33b0bb0903acd73a53c85
Joel Acquah <joelaj06@gmail.com>
2026-07-17 19:58:16 +0000

    modified wnp routes

--- lib/feature/auto_manager/presentation/work_pay/arguments/work_pay_arguments.dart
+++ lib/feature/auto_manager/presentation/work_pay/arguments/work_pay_arguments.dart
@@ -1,12 +1,17 @@
+import '../../../data/model/response/driver/driver_model.dart';
 import '../../../data/model/response/work_and_pay/work_and_pay_agreement_model.dart';
 
 class WorkAndPayArguments {
   WorkAndPayArguments({
     this.agreementId,
     this.workAndPayAgreement,
+    this.driverId,
+    this.driver,
   });
 
   final String? agreementId;
   final WorkAndPayAgreement? workAndPayAgreement;
+  final String? driverId;
+  final Driver? driver;
 }
 
